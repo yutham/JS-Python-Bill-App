dc948c75282201815424941e69cffe5da6af6a20
prasathsuriya <prasathsuriya1@gmail.com>
2023-09-10 14:57:32 +0530

    Comments added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
  
-import webview
+import webview # Libray to create a gui window
  
 app = Flask(__name__, static_folder='./static', template_folder='./templates')
  
@@ -12,8 +12,8 @@ def login():
 def home():
     return render_template("home.html",name='cairocoders page 2')
      
-webview.create_window('Billing App', app)
+webview.create_window('Billing App', app) # To enable a gui window
  
 if __name__ == '__main__':
-    # app.run(debug=True)
-    webview.start()
+    # app.run(debug=True) # Only use for development (recommended)
+    webview.start() # To start a gui
